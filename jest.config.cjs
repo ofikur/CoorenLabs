93b442b225277b1c3934a957e4fd99b88fa3e39a
RY4N <limitless.sensi@gmail.com>
2026-03-29 01:22:29 +0600

    chore: update jest test pattern to include both js and ts extensions

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
         "**/src/**/__tests__/**/*.[jt]s?(x)",
         "**/src/**/?(*.)+(spec|test).[jt]s?(x)",
         "!**/*.vitest.test.ts",
-        "**/scripts/tests/**/*.jest.test.cjs"
+        "**/scripts/tests/**/*.jest.test.[jt]s"
     ],
 
 
